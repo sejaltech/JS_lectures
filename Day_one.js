f3f9ad423f3483f9f14baf2559ae3ddf707a69b7
Sejal Singh <sejalsingh.ragini@gmail.com>
2026-01-06 19:27:58 +0530

    Add files via upload

---
+++ Day_one.js
@@ -0,0 +1,23 @@
+// console.log("Hello, World!");
+
+let fname = "sejal";
+fname[0] = "h";
+console.log(fname);
+
+console.log(fname.toUpperCase());
+
+let sayHello = "Hello";
+console.log(sayHello, fname);
+let great = sayHello + " " + fname;
+console.log(great);
+
+console.log(`${sayHello} ${fname}`);
+
+
+// let age = 17;
+// console.log(typeof age);
+
+
+"use strict";
+var placement = null; 
+console.log(placement);
---
+++ exercise one.js
@@ -0,0 +1,28 @@
+console.log("Hello! I am your coding fun fact guide!") //1. first question
+
+var botName = "Sejal"
+var botLocation = "India"
+var favoriteLanguage = "JavaScript"
+
+console.log(botName);
+console.log(botLocation);
+console.log(favoriteLanguage);   // second question
+
+
+console.log("My name is " + botName + " and I live in " + botLocation); // third question
+
+console.log("My favorite programming language is " + favoriteLanguage); // fourth question
+
+
+let codingFact = "sejal";
+console.log("That is a fun fact " + codingFact + "  about your bot's favorite language JavaScript"); // fifth answer
+
+
+console.log(codingFact);  // sixth answer
+
+
+
+codingFact = favoriteLanguage + " is used for both front-end and back-end development.";
+console.log(codingFact);
+
+ 
